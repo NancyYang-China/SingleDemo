caff95651ad68fa1c03f7346794fb08f7983eea6
xiaoming Jin <jin.xiaoming@ekohe.com>
2017-07-26 00:15:59 +0800

    MISC - header menu mobile

--- app/assets/javascripts/components/header_bar.es6.jsx
+++ app/assets/javascripts/components/header_bar.es6.jsx
@@ -1,6 +1,9 @@
 class HeaderBar extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      showModel: false
+    }
 
     this.menus = [{
       path: '/about_us',
@@ -20,7 +23,35 @@ class HeaderBar extends React.Component {
     }]
   }
 
+  _renderModal() {
+    const { showModel } = this.state;
+    const { path } = this.props;
+    if (!showModel) return null;
+
+    var itemsView = [{path: '/', name: "首页"}].concat(this.menus).map((menu, index)=> {
+      var color = path.indexOf(menu.path) < 0 ? "#000000" : "#a80309"
+      if (index === 0) {
+        color = path.length < 2 ? "#a80309" : "#000000"
+      }
+      return (
+        <a key={menu.path} style={{color}} className="menu-item" href={menu.path}>
+          <p className="title">{menu.name}</p>
+          <div className="line"/>
+        </a>
+      )
+    })
+
+    return (
+      <div className="header-modal"  onClick={()=> this.setState({showModel: false})}>
+        <div className="content flex-v">
+          {itemsView}
+        </div>
+      </div>
+    );
+  }
+
   render () {
+    const { showModel } = this.state;
     const { path } = this.props
     const menusView = this.menus.map((menu, index)=> {
       const color = path.indexOf(menu.path) < 0 ? "#000000" : "#a80309"
@@ -32,8 +63,13 @@ class HeaderBar extends React.Component {
     })
 
     var pp = path === "/news/show" ? '../' : ''
-
+    var menuIcon = showModel ? (
+      <span className="icon fa fa-close"></span>
+    ) : (
+      <span className="icon fa fa-bars"></span>
+    )
     return (
+      <div>
       <div className='top-bar'>
         <div className='forground'/>
         <div className='menu-content'>
@@ -43,7 +79,12 @@ class HeaderBar extends React.Component {
           <div className='menu-bar'>
             {menusView}
           </div>
+          <div className='menu-icon' onClick={()=> this.setState({showModel: !showModel})}>
+            {menuIcon}
+          </div>
         </div>
+      </div>
+      {this._renderModal()}
       </div>
     )
   }
