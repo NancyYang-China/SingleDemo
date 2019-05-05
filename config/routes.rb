41c0ee55914f2ebed1b7a863368701ae26f84548
xiaoming Jin <jin.xiaoming@ekohe.com>
2019-05-05 13:17:16 +0800

    Mainantice

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,16 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
 
-  root to: 'home#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-  resources :news, only: [:index, :show]
-  get :about_us, to: 'home#about_us', as: :about_us
-  get :investment, to: 'home#investment', as: :investment
-  get :shengzihui, to: 'home#shengzihui', as: :shengzihui
-  get :contact_us, to: 'contact_us#index', as: :contact_us
-  get :recruitment, to: 'recruitment#index', as: :recruitment
+  get '*path' => redirect('/')
+
+  #
+  # root to: 'home#index'
+  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #
+  # resources :news, only: [:index, :show]
+  # get :about_us, to: 'home#about_us', as: :about_us
+  # get :investment, to: 'home#investment', as: :investment
+  # get :shengzihui, to: 'home#shengzihui', as: :shengzihui
+  # get :contact_us, to: 'contact_us#index', as: :contact_us
+  # get :recruitment, to: 'recruitment#index', as: :recruitment
 end
