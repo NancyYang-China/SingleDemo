c5c89f13f7cb00ea2f77756e157fccff4ef2b906
NancyYang-China <275208592@qq.com>
2023-03-16 10:43:14 +0800

    Update ICP info

--- app/assets/javascripts/components/footer.es6.jsx
+++ app/assets/javascripts/components/footer.es6.jsx
@@ -91,12 +91,14 @@ class Footer extends React.Component {
       <div className='footer'>
         {menusView}
         <div className="bottom_info">
-          <p className="copyright">{`版权声明  Copyright@ 盛宇基金${new Date().getFullYear()} 沪ICP备06054643号-1`}</p>
+          <p className="copyright">{`版权声明  Copyright@ 盛宇基金${new Date().getFullYear()} `}
+            <a href="https://beian.miit.gov.cn/" target="_blank">沪ICP备06054643号-1</a>
+          </p>
           <div className="gab flex-h flex-hc">
-            <a className="gab_link" target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=31010402003542">
+            <a className="gab_link" target="_blank" href="https://www.beian.gov.cn/portal/registerSystemInfo?recordcode=31010402003542">
               <img className="gab_icon" src="assets/images/ghs.png"/>
               <p className="gab_title">沪公网安备 31010402003542号</p>
-            </a >
+            </a>
           </div>
         </div>
       </div>
--- app/assets/javascripts/components/statement.es6.jsx
+++ app/assets/javascripts/components/statement.es6.jsx
@@ -10,7 +10,9 @@ class Statement extends React.Component {
           <img src="assets/images/statement1.jpg" className="statement_image"/>
         </a>
         <div className="bottom_info">
-          <p className="copyright">{`版权声明  Copyright@ 盛宇基金${new Date().getFullYear()} 沪ICP备06054643号-1`}</p>
+          <p className="copyright">{`版权声明  Copyright@ 盛宇基金${new Date().getFullYear()} `}
+            <a href="https://beian.miit.gov.cn/" target="_blank">沪ICP备06054643号-1</a>
+          </p>
           <div className="gab flex-h flex-hc">
             <a className="gab_link" target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=31010402003542">
               <img className="gab_icon" src="assets/images/ghs.png"/>
